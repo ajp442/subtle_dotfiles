a8a681637f46c1e86b001caf18e5b9919f8e0976
Andrew Pierson <andrew.pierson@ravenind.com>
2021-04-15 13:50:19 -0500

    Rename .tmux.conf to tmux.conf

--- config.fish
+++ config.fish
@@ -7,6 +7,11 @@ set -x FZF_DEFAULT_OPTS --info=inline \
 "--preview 'batcat --style changes --color=always --line-range :60 {}'" \
 --bind='F2:toggle-preview'
 
+# Need how to figure out the best way to add $HOME/.local/bin to PATH
+#set -la PATH /home/ajp/.local/bin
+#set -Ua fish_user_paths $HOME/.local/bin
+#contains $HOME/.local/bin $fish_user_paths; or set -Ua fish_user_paths $HOME/.local/bin
+
 # Starting tmux from fish, got this from:
 # https://www.markhansen.co.nz/auto-start-tmux/
 # Adapted from https://github.com/fish-shell/fish-shell/issues/4434#issuecomment-332626369
@@ -19,3 +24,8 @@ and not set -q TMUX
   #tmux new-session -A -s main
   tmux
 end
+
+abbr setclip "xclip -selection c"
+abbr getclip "xclip -selection c -o"
+
+fish_vi_key_bindings

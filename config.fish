a310f8991434212e7be88db37573f553bcbf97d7
Andrew Pierson <ajp442@gmail.com>
2022-05-12 13:24:53 -0500

    Change fdfind to fd, change fzf preview

--- config.fish
+++ config.fish
@@ -1,10 +1,9 @@
-set -x FZF_DEFAULT_COMMAND "fdfind --hidden --follow --exclude '.git'"
+set -x FZF_DEFAULT_COMMAND "fd --hidden --follow --exclude '.git'"
 set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 
 set -x FZF_DEFAULT_OPTS --info=inline \
 --layout=reverse \
---preview-window=:hidden \
-"--preview 'batcat --style changes --color=always --line-range :60 {}'" \
+"--preview 'bat --style changes --color=always --line-range :60 {}'" \
 --bind='F2:toggle-preview'
 
 # Need how to figure out the best way to add $HOME/.local/bin to PATH
@@ -33,3 +32,17 @@ fish_vi_key_bindings
 source ~/.config/gitlint/gitlintrc.fish
 
 set fish_prompt_pwd_dir_length 0
+
+set -x FORGIT_COPY_CMD 'xclip -selection clipboard'
+set -x FORGIT_LOG_GRAPH_ENABLE true
+# This is from:
+# https://github.com/bigH/git-fuzzy
+# I'm not a huge fan so far, I might remove it.
+#set -x PATH /home/ajp/repos"/git-fuzzy/bin:$PATH"
+
+# This is from:
+# https://brettterpstra.com/2021/11/25/git-better-with-fzf-and-fish/
+# if status is-interactive && test -f ~/.config/fish/custom/git_fzf.fish
+# 	source ~/.config/fish/custom/git_fzf.fish
+# 	git_fzf_key_bindings
+# end

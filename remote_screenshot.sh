a19f6e187a0891f9446214aebfe0c721d69e6961
Andrew Pierson <ajp442@gmail.com>
2025-08-27 12:08:45 -0500

    Better printout after running remote_screenshot.sh

--- remote_screenshot.sh
+++ remote_screenshot.sh
@@ -3,10 +3,11 @@
 remoteScreenshot () 
 { 
     HOST=${1:-"root@192.168.0.10"};
-    IMG="${2:-"/home/root/screenshots/screenshot_$(date '+%Y-%m-%d_%H_%M_%S').png"}";
+    FILE_NAME="screenshot_$(date '+%Y-%m-%d_%H_%M_%S').png"
+    IMG="${2:-"/home/root/screenshots/screenshot_${FILE_NAME}"}";
     echo "Saving screenshot as $IMG";
     ssh "$HOST" "export DISPLAY=0:0; import -display 0:0 -window root '$IMG'";
-    echo "Downloading ${IMG} to ~/Pictures/";
+    echo "Downloading ${IMG} to ~/Pictures/${FILE_NAME}";
     scp "${HOST}:${IMG//\ /\\\ }" ~/Pictures/
 }
 
